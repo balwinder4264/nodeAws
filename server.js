b482772465c174c3b8c7709939f097db77831b98
balwinder4264 <balwindersingh4264@gmail.com>
2022-02-04 23:42:48 -0500

    test env varibale

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-
+require("dotenv").config();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the hpme page Baby ");
+    res.send(`Welcome to the hpme page Baby ${process.env.NAME}`);
 })
 const port = process.env.PORT || 3000
 app.listen(port, () => {
