e282b53a644210f6ef8454a38f52586d19420af4
balwinder4264 <balwindersingh4264@gmail.com>
2022-02-05 18:01:49 -0500

    adde new code

--- server.js
+++ server.js
@@ -1,12 +1,73 @@
-const express = require("express");
-const app = express();
+const path = require('path');
 require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
 
-app.get('/', (req, res) => {
-    res.send(`Welcome to the hpme page Baby ${process.env.name}`);
-})
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log("Server is listning")
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
 });
 
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+app.get('/test', () => {
+    return "heloo";
+})
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+console.log(" database =>", process.env.DB_REMOTE)
+const port = process.env.PORT || 8080
+mongoose
+    .connect(
+        process.env.DB_REMOTE
+    )
+    .then(result => {
+        app.listen(port);
+    })
+    .catch(err => console.log(err));
