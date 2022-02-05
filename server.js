54fb5f4b584baf690b9b03c68c24e01fd53ea4f3
balwinder4264 <balwindersingh4264@gmail.com>
2022-02-04 23:56:52 -0500

    chnaged name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 
 app.get('/', (req, res) => {
-    res.send(`Welcome to the hpme page Baby ${process.env.NAME}`);
+    res.send(`Welcome to the hpme page Baby ${process.env.name}`);
 })
 const port = process.env.PORT || 3000
 app.listen(port, () => {
