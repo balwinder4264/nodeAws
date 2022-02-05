1803c5f13476ab018407be2e802535a47f50d27b
balwinder4264 <balwindersingh4264@gmail.com>
2022-02-04 23:06:05 -0500

    port to PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("Welcome to the hpme page Baby 123");
 })
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log("Server is listning")
 });
